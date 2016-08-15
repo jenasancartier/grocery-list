9a25c9209addf8d3891d6eea122cc5b06afcaa4a
Jena SanCartier and Kyle Lange <jena@>
2016-08-15 16:33:50 -0700

    add initial javascript for working grocery list

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+// Business Logic
+
+
+
+// User Interface Logic
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+
+
+    $("form").hide();
+    $(".printList").show();
+
+
+  var groceries = ["g1","g2","g3","g4","g5","g6","g7","g8","g9","g10"];
+  // console.log(groceries);
+
+  groceries.forEach(function(grocery){
+    var list = $("input#" + grocery).val();
+    $("ol").append("<li>" + list + "</li>");
+  });
+
+
+
+  });
+});
